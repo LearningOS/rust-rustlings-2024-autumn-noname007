c3c2347632dead9668963fdaba3ff792d52413e4
noname007 <soul11201@gmail.com>
2024-10-20 01:45:30 +0800

    Resolve macros & clippy  & conversions stage

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,33 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() < 1 {
+            return Person::default();
+        }
+        let s_vec = s.split(",").collect::<Vec<_>>();
+
+        if s_vec.len() != 2{
+            return Person::default();
+        }
+
+        let name = s_vec[0];
+        if name.len() < 1 {
+            return Person::default();
+        }
+
+        let age = s_vec[1].parse::<usize>().ok();
+        if age.is_none(){
+            return Person::default();
+        }
+
+        return Person {
+            name: name.into(),
+            age: age.expect(""),
+        };
+        
     }
 }
 
